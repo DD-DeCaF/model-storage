a4d06e065cd23f8105042be7f0a5b8c0b90a1f9c
Ali Kaafarani <ali@kvikshaug.no>
2018-11-28 12:37:36 +0100

    feat: handle uncaught exceptions

--- src/model_storage/app.py
+++ src/model_storage/app.py
@@ -19,14 +19,13 @@ import json
 import logging
 import logging.config
 
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from raven.contrib.flask import Sentry
 from werkzeug.contrib.fixers import ProxyFix
-from werkzeug.exceptions import HTTPException
 
-from . import jwt, resources
+from . import errorhandlers, jwt, resources
 from .models import Model
 from .settings import current_config
 
@@ -59,21 +58,8 @@ def init_app(application, db):
     # Add JWT middleware
     jwt.init_app(application)
 
-    # Add an error handler for webargs parser error, ensuring a JSON response
-    # including all error messages produced from the parser.
-    @application.errorhandler(422)
-    def handle_webargs_error(error):
-        response = jsonify(error.data['messages'])
-        response.status_code = error.code
-        return response
-
-    # Handle werkzeug HTTPExceptions (typically raised through `flask.abort`) by
-    # returning a JSON response including the error description.
-    @application.errorhandler(HTTPException)
-    def handle_error(error):
-        response = jsonify({'message': error.description})
-        response.status_code = error.code
-        return response
+    # Register error handlers
+    errorhandlers.init_app(application)
 
     # Please keep in mind that it is a security issue to use such a middleware
     # in a non-proxy setup because it will blindly trust the incoming headers
